58a7eb1c52fd7c185eb827af7755fda976810555
Le Thanh Tam <93333830+Lethanhtam20003@users.noreply.github.com>
2026-06-29 12:37:39 +0700

    feat(fe): load data invoice 2 fe

--- user-web/src/app/(app)/(protected)/profile/invoices/page.tsx
+++ user-web/src/app/(app)/(protected)/profile/invoices/page.tsx
@@ -1,14 +1,12 @@
 import { JSX } from 'react';
-import { UserInvoice } from '@/types/invoices/user/UserInvoice';
 import UserInvoicesContainer from './_components/user-invoices-container';
-import { getUserInvoicesByUserId } from '@/services/invoices/user/invoice-service';
+import apiServer from '@/lib/api-server';
+import { InvoiceService } from '@/services/invoices/user/invoice-service';
 
 export default async function InvoicesPage(): Promise<JSX.Element> {
-	// TODO: Lấy userId thực tế từ Session/Token. Tạm thời hardcode là 1.
+	const invoiceService = new InvoiceService(apiServer);
 	const userId = '1a2b3c4d-1111-4aaa-8bbb-111111111111';
-
-	// Server-Side Fetching (RSC) với tham số userId
-	const initialInvoices = await getUserInvoicesByUserId(userId);
+	const initialInvoices = await invoiceService.getUserInvoicesByUserId(userId,{page:1,limit:1});
 
 	return (
 			<UserInvoicesContainer
--- user-web/src/queries/invoices/user/use-invoice-detail-query.ts
+++ user-web/src/queries/invoices/user/use-invoice-detail-query.ts
@@ -3,21 +3,24 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { InvoiceDetail } from '@/types/invoices/user/InvoiceDetail';
 import {
-	getInvoiceDetailByInvoiceId,
-	getUserInvoicesByUserId,
+	InvoiceService,
 } from '@/services/invoices/user/invoice-service';
 import { UserInvoice } from '@/types/invoices/user/UserInvoice';
 import { PaginationResponse } from '@/types/shared/PaginationResponse';
+import apiClient from '@/lib/api-client';
 
 export function useInvoiceDetailQuery(
-	invoiceId: number,
+	invoiceId: number | string,
 	initialData?: InvoiceDetail,
 ): UseQueryResult<InvoiceDetail, Error> {
+	const invoiceService = new InvoiceService(apiClient);
 	return useQuery({
 		queryKey: ['invoice-detail', invoiceId],
-		queryFn: (): Promise<InvoiceDetail> => getInvoiceDetailByInvoiceId(invoiceId),
+		queryFn: (): Promise<InvoiceDetail> =>
+			invoiceService.getInvoiceDetailByInvoiceId(invoiceId),
 		initialData: initialData, // Nhận data có sẵn từ Server để hiển thị ngay lập tức
-		enabled: invoiceId > 0, // Đổi điều kiện check vì giờ là kiểu number
+		enabled: !!invoiceId,
+		// enabled: invoiceId > 0, // Đổi điều kiện check vì giờ là kiểu number
 		staleTime: 1000 * 60 * 5, // Giữ data tươi trong 5 phút, tránh gọi lại API vô ích
 	});
 }
@@ -26,12 +29,13 @@ export function useUserInvoicesQuery(
 	userId: string,
 	initialData?: PaginationResponse<UserInvoice>,
 ): UseQueryResult<PaginationResponse<UserInvoice>, Error> {
+	const invoiceService = new InvoiceService(apiClient);
 	return useQuery({
 		// Đưa userId vào key để định danh bộ cache duy nhất cho user này
 		queryKey: ['user-invoices', userId],
 
 		// Sử dụng arrow function để truyền tham số vào service
-		queryFn: () => getUserInvoicesByUserId(userId),
+		queryFn: () => invoiceService.getUserInvoicesByUserId(userId),
 
 		initialData: initialData,
 
